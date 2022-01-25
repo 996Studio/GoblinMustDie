d56155837af07282ebdc32103b89f5dbc30126db
Fnu Yiliqi <khai.ilch167@gmail.com>
2022-01-25 10:40:55 -0500

    Created EnemyBase

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -14,28 +14,50 @@ public class EnemyBase : MonoBehaviour
     
     public bool isDead = false;
     
-    
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
     }
-
+    
     //Init monster Info
     //to be modified later by Jason data
     public void InitInfo()
     {
         maxHP = 100;
         curHP = 100;
-        
+        agent.speed = 3.5f;
+        agent.angularSpeed = 120f;
+        agent.acceleration = 8f;
     }
 
     void Start()
     {
-        
+        //agent.SetDestination();
     }
 
-    // Update is called once per frame
+    public void TakeDamage(int dmg)
+    {
+        curHP -= dmg;
+
+        if (curHP <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        isDead = true;
+        agent.isStopped = true;
+        Destroy(this);
+    }
+    
+    public void DeathEvent()
+    {
+        //Placeholder for animation event
+    }
+
     void Update()
     {
         
