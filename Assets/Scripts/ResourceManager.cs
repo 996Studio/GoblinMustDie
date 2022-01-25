936f71976e9b2250b9640c0df5a25402d6713cab
Luffy <luffyklose@gmail.com>
2022-01-25 11:05:55 -0500

    Add resource manager

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ResourceManager
+{
+    private static ResourceManager instance;
+    
+    private float coin;
+    private float wood;
+    private float rock;
+
+    private ResourceManager()
+    {
+        
+    }
+
+    public static ResourceManager Instance()
+    {
+        if (instance != null)
+        {
+            instance = new ResourceManager();
+        }
+
+        return instance;
+    }
+
+    public float Coin
+    {
+        get { return coin; }
+        set { coin = value; }
+    }
+
+    public float Wood
+    {
+        get { return wood; }
+        set { wood = value; }
+    }
+
+    public float Rock
+    {
+        get { return rock; }
+        set { rock = value; }
+    }
+
+    public void ChangeCoin(float number)
+    {
+        coin += number;
+    }
+
+    private void ChangeWood(float number)
+    {
+        wood += number;
+    }
+
+    private void ChangeRock(float number)
+    {
+        rock += number;
+    }
+}
