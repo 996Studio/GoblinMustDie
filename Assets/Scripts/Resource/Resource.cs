581e76998ea81620b098423ebd245c0cbb96e3a8
lazynorie <chengjy1991@gmail.com>
2022-02-02 11:14:51 -0500

    added resource

--- Assets/Scripts/Resource/Resource.cs
+++ Assets/Scripts/Resource/Resource.cs
@@ -1,14 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class Resource : MonoBehaviour
 {
-    public float RotationSpeed;
+    public Vector3 RotationSpeed;
+
+    public int ResourceAmount;
+    
+    public Color hoverColor;
+
+    protected Renderer rend;
+
+    protected Color originColor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        originColor = rend.material.color;
     }
 
     // Update is called once per frame
@@ -18,4 +29,17 @@ public abstract class Resource : MonoBehaviour
     }
 
     public abstract void CollectResource();
+
+    /*public abstract void OnMouseEnter();
+    
+    public abstract void OnMouseExit();*/
+    
+    /*public void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+    public void OnMouseExit()
+    {
+        rend.material.color = originColor;
+    }*/
 }
--- Assets/Scripts/Resource/Wood.cs
+++ Assets/Scripts/Resource/Wood.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Wood : Resource
@@ -7,17 +8,45 @@ public class Wood : Resource
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        originColor = rend.material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(RotationSpeed * Time.deltaTime);
     }
 
     public override void CollectResource()
     {
-        throw new System.NotImplementedException();
+        ResourceManager.Instance().Wood += ResourceAmount;
+        Debug.Log("Current Wood:" + ResourceManager.Instance().Wood);
     }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+        CollectResource();
+    }
+    
+    /*public override void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+    public override void OnMouseExit()
+    {
+        rend.material.color = originColor;
+    }*/
+    
+    public void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+    public void OnMouseExit()
+    {
+        rend.material.color = originColor;
+    }
+
+    
 }
