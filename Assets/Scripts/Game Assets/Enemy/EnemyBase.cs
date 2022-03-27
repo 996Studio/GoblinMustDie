c82ec67090af4bb9dff96036d2e079a4e8160721
Luffy <luffyklose@gmail.com>
2022-03-27 18:50:46 -0400

    Fix merge bug;

--- Assets/Scripts/Game Assets/Enemy/EnemyBase.cs
+++ Assets/Scripts/Game Assets/Enemy/EnemyBase.cs
@@ -3,36 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mime;
+using Enum;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class EnemyBase : MonoBehaviour
 {
     [SerializeField]
     protected Animator animator;
-    
     [SerializeField]
     protected NavMeshAgent agent;
+    protected Rigidbody rigidbody;
     
     protected float maxHP;
-    
     protected float curHP;
-    
     protected int atk;
-    
     protected float moveSpeed;
-
     protected int coinValue;
     
     [SerializeField] 
     private Image HPBarForeground;
 
     private float updateSpeedSec = 0.3f;
-    
     protected bool isDead = false;
     protected bool canTakeDamage;
     private float recycleMultiplier;
+    protected ElementComponent elementComponent;
+    
+    //Freeze Parameter
+    private float freezeCounter;
 
     [Header("Hit Effect")] 
     public ParticleSystem overloadeffect;
@@ -48,8 +49,7 @@ public class EnemyBase : MonoBehaviour
     
     public event Action<float> OnHealthChanged = delegate(float f) {  };
     
-    //Freeze Parameter
-    private float freezeCounter;
+    
     
     public int Atk
     {
@@ -65,6 +65,7 @@ public class EnemyBase : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
 
         canTakeDamage = true;
 
@@ -73,6 +74,8 @@ public class EnemyBase : MonoBehaviour
         
         HPBarForeground.fillAmount = 0.5f;
         agent.SetDestination(MotherBase.Instance.transform.position);
+        
+        elementComponent = GetComponent<ElementComponent>();
     }
     
     protected void Start()
@@ -83,6 +86,28 @@ public class EnemyBase : MonoBehaviour
     protected virtual void Update()
     {
         FreezeTimer();
+        
+        //Test code
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            elementComponent.ElementAttack(ElementEnum.Fire, 1.0f, 1, 5);
+            //Debug.Log("Fire attack");
+        }
+        if (Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            elementComponent.ElementAttack(ElementEnum.Water, 10.0f, 1, 5);
+            //Debug.Log("Water attack");
+        }
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            elementComponent.ElementAttack(ElementEnum.Ice, 1.0f, 1, 5);
+            //Debug.Log("Ice attack");
+        }
+        if (Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            elementComponent.ElementAttack(ElementEnum.Thunder, 10.0f, 1, 5);
+            //Debug.Log("Thunder attack");
+        }
     }
     
     public virtual void InitInfo()
@@ -95,6 +120,7 @@ public class EnemyBase : MonoBehaviour
         if (canTakeDamage)
         {
             curHP -= dmg;
+            Debug.Log($"damage {dmg} to {curHP}");
             
             ChangeHealth();
             
@@ -103,6 +129,11 @@ public class EnemyBase : MonoBehaviour
                 Death();
             }
         }
+    }
+    
+    public void ElementAttack(ElementEnum element, float amount, int power, int damage)
+    {
+        elementComponent.ElementAttack(element, amount, power, damage);
     }
 
     public void ChangeHealth()
@@ -156,19 +187,43 @@ public class EnemyBase : MonoBehaviour
             if (freezeCounter <= 0)
             {
                 freezeCounter = 0.0f;
-                StopFreeze();
+                StopSlowDown();
             }
         }
     }
 
-    public void StartFreeze(float freezeFactor, float duration)
+    public void StartSlowDown(float freezeFactor, float duration)
     {
-        agent.speed *= freezeFactor;
+        agent.speed = freezeFactor;
         freezeCounter = duration;
     }
 
-    public void StopFreeze()
+    public void StopSlowDown()
     {
+        agent.speed = moveSpeed;
+    }
+    
+    public void Overload(int damage)
+    {
+        TakeDamage(damage);
+        rigidbody.AddForce(Vector3.back * 100.0f);
+    }
+
+    public void ElectroCharged(int damage)
+    {
+        Debug.Log("Electro Charge");
+        TakeDamage(damage);
+    }
+
+    public void Freeze()
+    {
+        Debug.Log("Start freezing");
+        agent.speed = 0;
+    }
+
+    public void Unfreeze()
+    {
+        Debug.Log("Stop freezing");
         agent.speed = moveSpeed;
     }
 
@@ -244,8 +299,6 @@ public class EnemyBase : MonoBehaviour
                 break;
         }
     }
-    
- 
 }
 
 
